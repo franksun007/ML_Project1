65411bdca90eeafedadb956a2594e2d0786ff015
Chenfan Sun <chenfs@cs.washington.edu>
2016-10-17 14:31:13 +0200

    implemented logistic regression

--- src/python/franksun_function.py
+++ src/python/franksun_function.py
@@ -87,21 +87,47 @@ def ridge_regression(y, tx, lambda_):
     return np.linalg.solve(tx.T @ tx + lambda_ * i, tx.T @ y)
 
 
+# No idea why this function is here
 # Aiming to return the negative of the value of log-likelihood
 def compute_cost_logistic_regression(w_guess, y, tx):
     nll = np.log(1 + np.exp(-y @ w_guess.T @ tx))
     return nll
 
 
-# Return the corresponding gradient
-def compute_gradient_logistic_regression(w_guess, y, tx, gamma):
-    return 0
+# I would assume that this function actually implemented
+# the computation part of the gradient of logistic regression.
+#
+# TODO: UNTESTED
+# I have no idea whether it's correct or not, but we will figure it
+# out this week during the exercise section.
+#
+# This function supposed to call the above function.
+# However, I didn't invoke it. (It's according to last year's lab sheet etc.)
+#
+# This function implements the below equation:
+# g = \delta L / \delta w = -X.T [ sigma(Xw) - t ]
+#
+# Return the negative value of the gradient of the log-likelihood
+def compute_gradient_neg_log_likelihood(w_guess, y, tx):
+    g = -tx.T @ ((np.exp(tx) / (1 + np.exp(tx @ w_guess))) - y)
+    return g
 
 
 def logistic_regression(y, tx, gamma, max_iters):
+    w = 0  # TODO: I have no idea what the init value should be
+    # This function is following the fashion of gradient descent,
+    # we might factor out the code, but let's figure out if this
+    # function will work first.
+    for n_iter in range(max_iters):
+        # compute gradient and loss
+        gradient = compute_gradient_neg_log_likelihood(y, tx, w)
+        # update w by gradient
+        w = w - gamma * gradient
+        # store w and loss
+    return w
 
-    return 0
 
-
+# I barely have an idea on how to implement the regularized version of
+# logistic regression..
 def reg_logistic_regression(y, tx, lambda_, gamma, max_iters):
     return 0
