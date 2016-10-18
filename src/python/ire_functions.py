59aa8de4b5c39831337c6d3026586c9e5e95946a
Ireneu Pla <ireneup@gmail.com>
2016-10-18 10:31:36 +0200

    Add compute_gradient function and necessary imports

--- src/python/ire_functions.py
+++ src/python/ire_functions.py
@@ -1,16 +1,25 @@
+import numpy as np
+
+from ./src.python import costs
+
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    N = y.shape[0]
+    e = y - np.dot(tx, w)
+    return (-1 / N) * np.dot(tx.T, e)
+
+
 def gradient_descent(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    print(ws)
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
         gradient = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w)
+        loss = costs.compute_loss(y, tx, w)
         w -= gamma * gradient
         # store w and loss
-        ws.append(np.copy(w))
         losses.append(loss)
 
     return losses, ws
